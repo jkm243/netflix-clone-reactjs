b0514e1fe3054c13253c0c80d77fee54f2c1a128
Jkm-Pc-S <jacqueskatsuva@gmail.com>
2021-10-29 15:27:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey people</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/requests.js
@@ -0,0 +1,1 @@
+const API_KEY = "ce3a9919c08c2205f8f4f1a521bf4f68";
