0d210f041ec54920e1df3fb767676201bb07bfe6
Jkm-Pc-S <jacqueskatsuva@gmail.com>
2021-10-31 03:03:12 +0300

    Designing Rows

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey people</h1>
+      <h1>Hey people</h1>\
+      <Row title="" />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Row({ title }) {
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default Row;
