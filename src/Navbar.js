cba0460cd9cee99035a1797624ec24fda4cd8e8a
Jkm-Pc-S <jacqueskatsuva@gmail.com>
2021-10-31 18:48:21 +0300

    Navbar done

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import avatar from './avatar.png'
+import './Navbar.css'
 
-//TODO: Trouvez les lien des images: logo et avatar puis passer a la suite
+
 function Navbar() {
+
+    const [show,handleShow] = useState(false)
+
+    useEffect(()=>{
+        window.addEventListener("scroll", ()=>{
+            if (window.scrollY > 100){
+                handleShow(true)
+            }else handleShow(false)
+        })
+        return ()=>{
+            window.removeEventListener("scroll")
+        }   
+     },[])
+
     return (
-      <div className="nav">
-        {/* <img
+      <div className={`nav ${show && "nav_black"}`}>
+        <img
           className="nav_logo"
-          src="https://upload.wikimedia/commons/0/0f/"
+          src="https://cdn.freelogovectors.net/wp-content/uploads/2017/04/netflix-logo-500x134.png"
           alt="Netflix Logo"
         ></img>
 
         <img
-          className="nav_logo"
-          src="https://upload.wikimedia/commons/0/0f/"
+          className="nav_avatar"
+          src={avatar}
           alt="Avatar logo"
-        ></img> */}
+        ></img>
       </div>
     );
 }
